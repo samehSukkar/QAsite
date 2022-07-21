ee52808446b4388eb2e48825ee6b47945dd436f3
Sameh Sukkar <sameh.sokkar2002@gmail.com>
2022-07-22 01:58:43 +0300

    create models

--- QA/models.py
+++ QA/models.py
@@ -1,3 +1,26 @@
+import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Question(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE )
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    publish_date = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE )
+    text = models.TextField()
+    publish_date = models.DateTimeField(default=datetime.datetime.now)
+
+
+
