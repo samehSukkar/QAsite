346bdc2123c1b6d79240952902d3ee14207e9c8f
Sameh Sukkar <sameh.sokkar2002@gmail.com>
2022-07-22 04:18:03 +0300

    add url and view

--- QA/admin.py
+++ QA/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Question , Answer
 # Register your models here.
+
+
+admin.site.register(Answer)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+   
+           list_display = ('title', 'publish_date')
+
+
+
+admin.site.register(Question,QuestionAdmin)
--- QA/models.py
+++ QA/models.py
@@ -1,6 +1,6 @@
-import datetime
 from django.db import models
 from django.contrib.auth.models import User
+import datetime 
 
 # Create your models here.
 
