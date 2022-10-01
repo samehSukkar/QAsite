b83752602103fbeacc9cc4aca8e0d17078efe805
Sameh Sukkar <sameh.sokkar2002@gmail.com>
2022-10-01 13:04:38 +0300

    first_version

--- QAsite/settings.py
+++ QAsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aoqyo9if5f78t#d!g3z&&(pwvt3g!ben)3t-)26ph&^%4^avbl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',]
 
